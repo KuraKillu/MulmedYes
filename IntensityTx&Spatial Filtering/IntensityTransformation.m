9a0c2ec820ee52f0fc454b70ba7d5892f4bbec68
KuraKillu <sdharmasaputrasoeroso@gmail.com>
2025-03-07 11:24:01 +0700

    Menambahkan log transform

--- IntensityTx&Spatial Filtering/IntensityTransformation.m
+++ IntensityTx&Spatial Filtering/IntensityTransformation.m
@@ -6,7 +6,8 @@ image = imread('image.jpg');
 
 % Variabel-variabel yang digunakan
 L = 256; % Jumlah maks level intensitas (8-bit = 256)
-c = 1; % Konstanta c
+cGamma = 1; % Konstanta c untuk gamma transform (bernilai 1 untuk normalisasi)
+cLog = 45; % Konstanta c untuk Log Transform (harus bernilai 0-255 karena gambar 8-bit)
 
 % Besar konstanta gamma
 gammaLight = 0.3; % Lebih kecil = lebih terang
@@ -16,13 +17,16 @@ gammaDark = 1.69;  % Lebih besar = lebih gelap
 negativeImage = (L - 1) - image;
 
 % Mengubah gamma dari gambar menjadi lebih terang dan gelap
-imageDouble = double(image) / (L - 1);
-imageLight = uint8(c * (imageDouble .^ gammaLight) * (L - 1)); % Lebih terang
-imageDark = uint8(c * (imageDouble .^ gammaDark) * (L - 1));   % Lebih gelap
+imageGamma = double(image) / (L - 1);
+imageLight = uint8(cGamma * (imageGamma .^ gammaLight) * (L - 1)); % Lebih terang
+imageDark = uint8(cGamma * (imageGamma .^ gammaDark) * (L - 1));   % Lebih gelap
+
+% Log Transform
+imageLog = uint8(cLog * log(1 + double(image)));
 
 % Menampilkan gambar asli dan hasil-hasil perubahan
-figure;
-subplot(2,2,1), imshow(image), title('Gambar Asli');
-subplot(2,2,2), imshow(negativeImage), title('Gambar Negatif');
-subplot(2,2,3), imshow(imageLight), title('Gambar Terang');
-subplot(2,2,4), imshow(imageDark), title('Gambar Gelap');
+figure; imshow(image); title('Gambar Asli');
+figure; imshow(negativeImage); title('Gambar Negatif');
+figure; imshow(imageLight); title('Gambar Terang');
+figure; imshow(imageDark); title('Gambar Gelap');
+figure; imshow(imageLog); title('Log Transform');
